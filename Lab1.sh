ecb90b55e4599057d877a711af2e2549d08c8b72
Abdelazez <3bdelazez900@gmail.com>
2023-08-08 20:37:03 +0300

    sed assignments solve

---
+++ Lab1.sh
@@ -0,0 +1,13 @@
+# solve Lab1 bash script
+#1
+cat /etc/passwd |grep -i lpg
+#2 display the /etc/passwd except the third line.
+#sed '3d' /etc/passwd =delete the third line
+awk -F: '{ if (NR!=3) print bash }' /etc/passwd
+#3 delete the last line.
+sed '$d' /etc/passwd
+#4 display /etc/passwd except the line that contain the word "lp"
+sed '/lp/d' /etc/passwd
+#5 substitute all the words that contain 'lp' with 'mylp' in /etc/passwd 
+#note g is an option mean substitute globaly .
+sed 's/lp/mylp/g' /etc/passwd
